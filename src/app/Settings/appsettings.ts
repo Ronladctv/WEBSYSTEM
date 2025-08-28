e57f4c0c62bdffffed379a5c6ee76f608cf51be7
Ronald Vinicio Carguaquispe Titoaña <ronald.carguaquispe@hotmail.com>
2025-08-28 01:45:56 -0500

    ESTABLE PR14

--- src/app/Settings/appsettings.ts
+++ src/app/Settings/appsettings.ts
@@ -1,4 +1,5 @@
 export const settings = {
     production: false,
-    endPoint: 'https://localhost:7251/'
+    //endPoint: 'https://localhost:7251/'
+    endPoint: 'https://webapitesis-hzgmezhdfjgcbygb.brazilsouth-01.azurewebsites.net/'
 };
