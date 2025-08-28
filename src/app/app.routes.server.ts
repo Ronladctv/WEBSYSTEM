9fd725c567acd82d86cc4102c2daa5f3050d6736
Ronald Vinicio Carguaquispe Titoaña <ronald.carguaquispe@hotmail.com>
2025-08-28 00:20:18 -0500

    ESTABLE PR6

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,24 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'security', renderMode: RenderMode.Prerender },
+  { path: 'inicio', renderMode: RenderMode.Prerender },
+  { path: 'perfil', renderMode: RenderMode.Prerender },
+  { path: 'home', renderMode: RenderMode.Prerender },
+  { path: 'cliente', renderMode: RenderMode.Prerender },
+  { path: 'historyVentas', renderMode: RenderMode.Prerender },
+  { path: 'usuario', renderMode: RenderMode.Prerender },
+  { path: 'proveedor', renderMode: RenderMode.Prerender },
+  { path: 'empresa', renderMode: RenderMode.Prerender },
+  { path: 'dashboard', renderMode: RenderMode.Prerender },
+  { path: 'configuracion', renderMode: RenderMode.Prerender },
+  { path: 'producto', renderMode: RenderMode.Prerender },
+  { path: 'venta', renderMode: RenderMode.Prerender },
+  { path: 'menu', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'security', renderMode: RenderMode.Prerender },
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: '**', renderMode: RenderMode.Prerender }
 ];
