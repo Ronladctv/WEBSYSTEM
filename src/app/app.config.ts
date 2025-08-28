8e644eddd374d952f2619718d26f26570d25439d
Ronald Vinicio Carguaquispe Titoaña <ronald.carguaquispe@hotmail.com>
2025-08-28 00:04:10 -0500

    ESTABLE PR5

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { materialProviders } from './shared-ui.js';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor} from './Custom/auth-interceptor.js'
+import { authInterceptor} from './Custom/auth-interceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
